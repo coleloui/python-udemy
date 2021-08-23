504449c96e8332c602c046a5a6c3fe83f1ae9410
coleloui <coleloui18@gmail.com>
2021-08-23 11:02:02 -0700

    testing

--- hi.py
+++ hi.py
@@ -10,7 +10,7 @@ type(a)
 
 print(a)
 
-my_income = 100
-tax_rate = 0.1
-my_taxes = my_income * tax_rate
-print(my_taxes)
+# my_income = 100
+# tax_rate = 0.1
+# my_taxes = my_income * tax_rate
+# print(my_taxes)
